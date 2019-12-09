0b37e12be8eaab3516ac2ab25770a8313df44bfb
Michael Jordan <Insurikai@gmail.com>
2019-12-09 12:26:40 -0800

    Added Queries

--- server.js
+++ server.js
@@ -6,4 +6,22 @@ const server = express();
 
 server.use(express.json());
 
+//write API endpoints to manage the `accounts` resource, including database logic
+
+//GET
+server.get('/', (req,  res)=>{
+  db('accounts').then(res.status(200).send)
+})
+//POST
+server.post('/', (req,  res)=>{
+  
+})
+//PUT
+server.put('/', (req,  res)=>{
+  
+})
+//DELETE
+server.delete('/', (req,  res)=>{
+  
+})
 module.exports = server;
